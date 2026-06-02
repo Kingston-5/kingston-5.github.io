c76fb0dcae656fd16a056fceae56ef2eee087442
kingston-5 <qhawem54@gmail.com>
2026-06-02 13:04:02 +0200

    implemented work filter

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -2,37 +2,43 @@ $(document).ready(function(){
 
 
 	let currentIndex = 0;
-	function showSlide(index) {
-		if (index < 0) {
-			currentIndex = $(".carousel-slide").length - 1;
-		} else if (index >= $(".carousel-slide").length) {
-			currentIndex = 0;
-		}
+function showSlide(index) {
+    const totalSlides = $(".carousel-slide").length;
 
-		$(".carousel-slides").css("transform", `translateX(-${currentIndex * 100}%)`);
+    if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (index >= totalSlides) {
+        currentIndex = 0;
+    } else {
+        currentIndex = index;
+    }
 
-	}
+    $(".carousel-slides").css(
+        "transform",
+        `translateX(-${currentIndex * 100}%)`
+    );
+}
 
 
-	$("#next").on('click', function() {
-		console.log("click");
-		currentIndex++;
-		showSlide(currentIndex);
-	});
+	
+	$(document).on('click', '#next', function() {
+    currentIndex++;
+    showSlide(currentIndex);
+});
 
-	$("#prev").click(function() {
-		currentIndex--;
-		showSlide(currentIndex);
-	});
+$(document).on('click', '#prev', function() {
+    currentIndex--;
+    showSlide(currentIndex);
+});
 
 	const autoAdvanceInterval = 3000; // Change slide every 3 seconds
 
 
-	setInterval(function() {
+	/**setInterval(function() {
 console.log("interval");
 		currentIndex++;
 		showSlide(currentIndex);
-	}, autoAdvanceInterval);
+	}, autoAdvanceInterval);*/
 });
 
 
--- scripts/gallery.js
+++ scripts/gallery.js
@@ -4,7 +4,8 @@ const projects = [
 	{
 		"id": 0,
 		"title": "Lihawu Health Screening",
-		"subtitle": "Professional | Ummo",
+		category: "company-work",
+		"subtitle": "Company Work | Ummo",
 		"description": "a health testing system implemented at ports of entry in Eswatini to monitor and manage public health risks. It focuses on screening travelers for diseases and ensuring safe cross-border transportation to protect the health of the population.",
 		"link": "this is the link",
 		"images": ["./images/lihawu/1.png", "./images/lihawu/2.png"],
@@ -15,7 +16,8 @@ const projects = [
 	{
 		"id": 1,
 		"title": "Sunrise eCommerce",
-		"subtitle": "Professional | Centurion (pty) Ltd",
+		category: "company-work",
+		"subtitle": "Company Work | Centurion (pty) Ltd",
 		"description": "Sunrise Hub is a digital service marketplace by Centurion Technologies. We connect individuals, MSMEs & communities to trusted services. From eKYC,eCommerce, digital marketing, certification to payments-all in one place",
 		"link": "https://www.facebook.com/sunrisehub/",
 		"images": ["./images/sunrise/1.png", "./images/sunrise/2.png", "./images/sunrise/3.png", "./images/sunrise/4.png", "./images/sunrise/5.png", "./images/sunrise/6.png"],
@@ -24,7 +26,8 @@ const projects = [
 	{
 		"id": 2,
 		"title": "Kwakhanya PlantIQ",
-		"subtitle": "Entrepreneurial | client",
+		category: "freelance",
+		"subtitle": "Freelance",
 		"description": "Kwakhanya PlantIQ provides n affordable, hybrid smart irrigation system. key features included offline irrigation management, smart fertigation(automatic fertiliser despensor) push notifications water flow monitoring & analysis and machine learnign powered predictive scheduling.",
 		"link": "",
 		"images": ["./images/kwakhanya/1.jpeg", "./images/kwakhanya/2.jpeg", "./images/kwakhanya/3.jpeg"],
@@ -33,7 +36,8 @@ const projects = [
 	{
 		"id": 3,
 		"title": "Sakhisizwe Tutor App",
-		"subtitle": "Entrepreneurial | client",
+		category: "freelance",
+		"subtitle": "Freelance",
 		"description": "Sakhisizwe is an offline first desktop student tutor application designed to help improve access to digital learnign materials for students even without access to the internet.",
 		"link": "",
 		"images": ["./images/sakhisizwe/1.png", "./images/sakhisizwe/2.png", "./images/sakhisizwe/3.png", "./images/sakhisizwe/4.png", "./images/sakhisizwe/5.png", "./images/sakhisizwe/6.png"],
@@ -43,7 +47,8 @@ const projects = [
 	{
 		"id": 4,
 		"title": "Church Management System",
-		"subtitle": "Entrepreneurial | client",
+		category: "freelance",
+		"subtitle": "Freelance",
 		"description": "Streamlined member registration, profiles, attendance tracking, and communication. Centralized and automated financial transactions, contributions, and reporting for improved transparency and compliance.",
 		"link": "",
 		"images": ["./images/chms/1.png", "./images/chms/2.png", "./images/chms/3.png", "./images/chms/4.png", "./images/chms/5.png", "./images/chms/6.png"],
@@ -53,7 +58,8 @@ const projects = [
 	{
 		"id": 5,
 		"title": "Fundmo",
-		"subtitle": "Entrepreneurial | Fundmo (pty) Ltd",
+		category: "ventures",
+		"subtitle": "Ventures | Fundmo (pty) Ltd",
 		"description": "startup vibes",
 		"link": "",
 		"images": ["./images/fundmo/1.jpg", "./images/fundmo/2.jpg", "./images/fundmo/3.jpg"],
@@ -63,16 +69,18 @@ const projects = [
 	{
 		"id": 6,
 		"title": "Centralised Mobile Money API",
+		category: "academic",
 		"subtitle": "Academic | UNESWA B.Sc I.T",
 		"description": "The developed unified API offers a streamlined approach, facilitating financial service access and supporting Eswatini's FinTech ecosystem growth.",
 		"link": "",
-		"images": ["./images/api/1.png", "./images/fundmo/2.png", "./images/fundmo/3.png"],
+		"images": ["./images/api/1.png", "./images/api/2.png", "./images/api/3.png"],
 
 	},
 {
 		"id": 7,
 		"title": "Swatini.shop",
-		"subtitle": "Entrepreneurial | client",
+		category: "freelance",
+		"subtitle": "Freelance",
 		"description": "swatini.shop was a C2C ecommerce platform that enabled users to quickly list their pre-loved items for. key features included live chat, quick listing.",
 		"link": "",
 		"images": ["./images/swatini/1.png", "./images/swatini/2.png", "./images/swatini/3.png", "./images/swatini/4.png"],
@@ -80,41 +88,103 @@ const projects = [
 	}
 ]
 
+function closeModal() {
+    $('.modal-content').empty();
+    $('#modalDialog').removeClass('active');
+    
+}
+
+function renderGallery(filter = "all") {
+
+    $('#gallery').empty();
+
+    const filteredProjects =
+        filter === "all"
+            ? projects
+            : projects.filter(
+                p => p.category === filter
+            );
+
+    filteredProjects.forEach((p) => {
+
+        $('#gallery').append(`
+            <div class="card" data-id="${p.id}">
+                <img src="${p.images[0]}" alt="">
+                <div class="container">
+                    <h4><b>${p.title}</b></h4>
+                    <p>${p.subtitle}</p>
+                </div>
+            </div>
+        `);
+
+    });
+}
 
 $(document).ready(function(){
 
-	projects.forEach((p) => {
 
-		$("#gallery").append(`
-						<div class="card" data-id="${p.id}">
-							<img src="${p.images[0]}" alt="Avatar" style="width:100%">
-							<div class="container">
-								<h4><b>${p.title}</b></h4>
-								<p>${p.subtitle}</p>
-							</div>
-						</div>`);
+renderGallery();
 
-	});
+// filter click
+$('.filter').on('click', function() {
+
+    $('.filter').removeClass('active');
+
+    $(this).addClass('active');
+
+    const filter = $(this).data('filter');
+    console.log("filter=>", filter);
+
+    renderGallery(filter);
+});
+
+//auto-generate counts
+
+$('#all-count').text(projects.length);
+
+$('#company-work-count').text(
+    projects.filter(
+        p => p.category === 'company-work'
+    ).length
+);
+
+$('#freelance-count').text(
+    projects.filter(
+        p => p.category === 'freelance'
+    ).length
+);
+
+$('#ventures-count').text(
+    projects.filter(
+        p => p.category === 'ventures'
+    ).length
+);
+
+$('#academic-count').text(
+    projects.filter(
+        p => p.category === 'academic'
+    ).length
+);
 
 	// When the user clicks the button, open the modal 
 	$(".card").on('click', function(e) {
 		let pId = $(this).data('id');
 		let project = projects[pId];
+		const slides = project.images.map((img, index) => `
+    <div class="carousel-slide">
+        <img src="${img}" alt="Image ${index + 1}">
+    </div>
+`).join('');
 
 
-		$('.modal-content').append(`	
+		$('.modal-content').html(`	
 						<div class="modal-main">
 
 							<div class="carousel-container">
 
 								<div class="carousel">
 									<div class="carousel-slides">
-									<div class="carousel-slide">
-										<img src="${project.images[0]}" alt="Image 1">
-									</div>
-									<div class="carousel-slide">
-										<img src="${project.images[1]}" alt="Image 2">
-									</div>
+									${slides}
 
 									</div>
 									<div class="carousel-controls">
@@ -129,11 +199,11 @@ $(document).ready(function(){
 							<div class="modal-text">
 								<h4><b>${project.title}</b></h4>
 								<p>${project.subtitle}</p>
-								<p>${project.description}
+								<p>${project.description}</p>
 							</div>
 							<div class="modal-controls">
 
-								<a href="${project.link}" about="__blank" type="button" class="close swipe-btn">
+								<a href="${project.link}" target="_blank" type="button" class="close swipe-btn">
 									<span aria-hidden="true">More info</span>
 								</a>
 									<button type="button" class="close swipe-btn">
@@ -148,22 +218,19 @@ $(document).ready(function(){
 
 		// When the user clicks on <span> (x), close the modal
 		$('.close').on('click', function() {
-			$('#modalDialog').fadeOut();
-
-			$('.modal-content').empty();
+			closeModal()
 
 		});
 
-		$('#modalDialog').show();
+		$('#modalDialog').addClass('active');
 	});
 
 
 
 	// When the user clicks anywhere outside of the modal, close it
-	$('main').on('click', function(e){
-
-		if($(e.target).hasClass("modal")){
-			$('#modalDialog').fadeOut();
-		}
-	});
+$('#modalDialog').on('click', function(e) {
+    if (e.target === this) {
+        closeModal()
+    }
+});
 });

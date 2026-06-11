1fed35c5912ee95277b8468ecb30f2025ade4981
kingston-5 <qhawem54@gmail.com>
2026-06-11 18:58:08 +0200

    add improved fonts

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -4,7 +4,7 @@ const projects = [
 	{
 		"id": 0,
 		"title": "Lihawu Health Screening",
-		category: "company-work",
+		"category": "company-work",
 		"subtitle": "Company Work | Ummo",
 		"description": "A Minitsy of Health initiative implemented at ports of entry in Eswatini to monitor and manage public health risks. It focuses on screening travelers for diseases and ensuring safe cross-border transportation to protect the health of the population.",
 		"link": "this is the link",
@@ -16,7 +16,7 @@ const projects = [
 	{
 		"id": 1,
 		"title": "Sunrise eCommerce",
-		category: "company-work",
+		"category": "company-work",
 		"subtitle": "Company Work | Centurion (pty) Ltd",
 		"description": "Sunrise Hub is a digital service marketplace by Centurion Technologies. We connect individuals, MSMEs & communities to trusted services. From eKYC,eCommerce, digital marketing, certification to payments-all in one place",
 		"link": "https://www.facebook.com/sunrisehub/",
@@ -26,7 +26,7 @@ const projects = [
 	{
 		"id": 2,
 		"title": "Kwakhanya PlantIQ",
-		category: "freelance",
+		"category": "freelance",
 		"subtitle": "Freelance",
 		"description": "Kwakhanya PlantIQ provides n affordable, hybrid smart irrigation system. key features included offline irrigation management, smart fertigation(automatic fertiliser despensor) push notifications water flow monitoring & analysis and machine learnign powered predictive scheduling.",
 		"link": "",
@@ -36,7 +36,7 @@ const projects = [
 	{
 		"id": 3,
 		"title": "Sakhisizwe Tutor App",
-		category: "freelance",
+		"category": "freelance",
 		"subtitle": "Freelance",
 		"description": "Sakhisizwe is an offline first desktop student tutor application designed to help improve access to digital learnign materials for students even without access to the internet.",
 		"link": "",
@@ -47,7 +47,7 @@ const projects = [
 	{
 		"id": 4,
 		"title": "Superb Choice Brands",
-		category: "freelance",
+		"category": "freelance",
 		"subtitle": "Freelance",
 		"description": "Superb choice brands celebrates the beauty and quality of Eswatini craftsmanship. Every product tells a story of local talent, sustainable practices, and community empowerment.",
 		"link": "https://superbchoicebrands.net/",
@@ -57,7 +57,7 @@ const projects = [
 	{
 		"id": 5,
 		"title": "Eswatini Verbal Arts Assembly",
-		category: "freelance",
+		"category": "freelance",
 		"subtitle": "Freelance",
 		"description": "At Eswatini Verbal Assembly, we believe every voice has the power to inspire, influence, and ignite change. Through comprehensive training, dedicated mentorship, and competitive opportunities, we cultivate the next generation of eloquent leaders.",
 		"link": "https://eswatiniverbalarts.org/",
@@ -67,7 +67,7 @@ const projects = [
 	{
 		"id": 6,
 		"title": "Church Management System",
-		category: "freelance",
+		"category": "freelance",
 		"subtitle": "Freelance",
 		"description": "Streamlined member registration, profiles, attendance tracking, and communication. Centralized and automated financial transactions, contributions, and reporting for improved transparency and compliance.",
 		"link": "",
@@ -78,7 +78,7 @@ const projects = [
 	{
 		"id": 7,
 		"title": "Fundmo",
-		category: "ventures",
+		"category": "ventures",
 		"subtitle": "Ventures | FundMo (pty) Ltd",
 		"description": "Co-founded a financial technology startup that developed a mobile money-based crowdfunding platform to help other Startups and social projects to secure the necessary funding they require to make an impact in this world. This project was initiated after a 1st place victory in the MTN MoMo Hackathon.",
 		"link": "",
@@ -89,7 +89,7 @@ const projects = [
 	{
 		"id": 8,
 		"title": "Centralised Mobile Money API",
-		category: "academic",
+		"category": "academic",
 		"subtitle": "Academic | UNESWA B.Sc I.T",
 		"description": "Inspired by my work on FundMo, I developed unified API offers a streamlined approach to integrating with multiple Mobile Money providers allowing small scale developers to skip the red tape and go straight to production, facilitating financial service access and supporting Eswatini's FinTech ecosystem growth.",
 		"link": "",
@@ -99,7 +99,7 @@ const projects = [
 {
 		"id": 9,
 		"title": "Swatini.shop",
-		category: "freelance",
+		"category": "freelance",
 		"subtitle": "Freelance",
 		"description": "swatini.shop was a C2C ecommerce platform that enabled users to quickly list their pre-loved items for. key features included live chat, quick listing.",
 		"link": "",
@@ -131,8 +131,8 @@ function renderGallery(filter = "all") {
             <div class="card" data-id="${p.id}">
                 <img src="${p.images[0]}" alt="">
                 <div class="container">
-                    <h4><b>${p.title}</b></h4>
-                    <p>${p.subtitle}</p>
+                    <h4 class="quantico-bold">${p.title}</h4>
+                    <p class="quantico-regular">${p.subtitle}</p>
                 </div>
             </div>
         `);
@@ -217,9 +217,9 @@ $('#academic-count').text(
 						</div>
 						<div class="modal-footer">
 							<div class="modal-text">
-								<h4><b>${project.title}</b></h4>
-								<p>${project.subtitle}</p>
-								<p>${project.description}</p>
+								<h4 class="quantico-bold">${project.title}</h4>
+								<p class="quantico-italic">${project.subtitle}</p>
+								<p class="quantico-regular">${project.description}</p>
 							</div>
 							<div class="modal-controls">
 
